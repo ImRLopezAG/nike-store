61cec37e2c3aa8812366e1a067571bd2e8ab54f9
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-07-07 12:23:32 -0400

    refactor: Improve card service and generateHolder function

--- src/service/card.ts
+++ src/service/card.ts
@@ -1,26 +1,23 @@
-export const cards: Record<string, RegExp> = {
+export const cards = {
   Visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
   MasterCard: /^5[1-5][0-9]{14}$/,
   'American Express': /^3[47][0-9]{13}$/,
   Discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/,
 }
+type CardHolder = KeyOf<typeof cards>;
 export const generateHolder = () => {
-  let cardNumber: string;
-  let cardHolder: string;
-
+  let cardNumber: string = '';
+  let cardHolder: CardHolder = 'Visa';
+  const loop = true;
   const next = (a: number, b: number) =>
     Math.floor(Math.random() * (b - a + 1) + a);
 
-  do {
-    cardNumber = Array.from({ length: 16 }, () => next(0, 9).toString()).join(
-      ''
-    );
-    cardHolder =
-      Object.keys(cards).find((key) =>
-        cards[key].test(cardNumber.replace(/\s/g, ''))
-      ) ?? 'Unknown';
-  } while (cardHolder === 'Unknown');
-
+  while (loop) {
+    cardNumber = Array.from({ length: 16 }, () => next(0, 9).toString()).join('');
+    cardHolder = (Object.keys(cards).find((card) => cards[card as CardHolder].test(cardNumber)) ?? 'Visa') as CardHolder;
+    if (cards.MasterCard.test(cardNumber) || cards.Visa.test(cardNumber) || cards['American Express'].test(cardNumber) || cards.Discover.test(cardNumber)) break;
+  }
+  
   return {
     number: cardNumber.match(/.{1,4}/g)?.join(' ') ?? '',
     cardNumber,
