9ecea66a38b843cd4e1b7d0dbc971badf89e45ff
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-07-07 11:14:36 -0400

    Refactor code paths, update imports, and add customer details to cart schema

--- src/shared/schemas/cart.schema.ts
+++ src/shared/schemas/cart.schema.ts
@@ -1,5 +1,12 @@
 import { z } from 'zod';
 import { ProductSchema } from './product.schema';
+const CardSchema = z.object({
+  number: z.string().length(16),
+  cvc: z.string().length(3),
+  month: z.string().length(2),
+  year: z.string().length(2),
+  holder: z.enum(['Visa', 'MasterCard', 'American Express', 'Discover']),
+});
 
 const TotalsSchema = z.object({
   products: z.number(),
@@ -8,10 +15,25 @@ const TotalsSchema = z.object({
   total: z.number(),
 });
 
+const CustomerSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  address: z.string(),
+  city: z.string(),
+  state: z.string(),
+  zip: z.string(),
+  country: z.string(),
+  phone: z.string(),
+  card: CardSchema,
+});
+
 export const CartSchema = z.object({
-  id: z.string(),
-  lines: z.array(ProductSchema.extend({
-    quantity: z.number().default(1),
-  })),
+  id: z.string().nanoid(),
+  lines: z.array(
+    ProductSchema.extend({
+      quantity: z.number().default(1),
+    })
+  ),
   totals: TotalsSchema,
+  customer: CustomerSchema,
 });
