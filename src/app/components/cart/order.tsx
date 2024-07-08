2d8f44be09fac33608b73cca211b45f754752047
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-07-08 07:08:04 -0400

    refactor: Update order details component to display shipping value

--- src/app/components/cart/order.tsx
+++ src/app/components/cart/order.tsx
@@ -4,22 +4,23 @@ import { Separator } from '@ui/separator';
 import { PaymentMethod } from './payment';
 import { Button } from '@ui/button';
 import { generateInvoice } from '@services/pdf'
-
+import { cartService } from '@services/cart'
 export const OrderDetails = () => {
   const { cart } = useCartStore((state) => state);
-
-  const { subtotal, totalVAT, total } = cart.totals;
+  const {f} = cartService()
+  const { subtotal, totalVAT, total, shipping } = cart.totals;
 
   return (
-    <Card className='w-full max-w-md'>
+    <Card className='w-full min-w-md max-w-lg'>
       <CardTitle className='text-4xl text-center text-blue-500'>
         Order details
       </CardTitle>
       <CardContent className='flex flex-col gap-2'>
-        <Cell title='Subtotal' value={`$${subtotal}`} />
-        <Cell title='VAT' value={`$${totalVAT}`} />
+        <Cell title='Subtotal' value={f(subtotal)} />
+        <Cell title='VAT' value={f(totalVAT)} />
+        <Cell title='Shipping' value={f(shipping)} />
         <Separator />
-        <Cell title='Total' value={`$${total}`} />
+        <Cell title='Total' value={f(total)} />
       </CardContent>
       <CardFooter className='flex gap-2'>
         <PaymentMethod />
